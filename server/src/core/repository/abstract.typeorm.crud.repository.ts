288a39040674c471f5f11078d8abd14ce590ef6e
Lucas Do Prado <93302128+LucksBr@users.noreply.github.com>
2026-04-27 23:27:09 -0300

    Creation of an abstract mode for creating typeORM repositories and a basic interface for crud operations on repositories

---
+++ server/src/core/repository/abstract.typeorm.crud.repository.ts
@@ -0,0 +1,30 @@
+import AbstractEntity from "../model/abstract.entity";
+import { DeepPartial, Repository } from "typeorm";
+import BaseRepositoryInterface from "./base.crud.repository.interface";
+
+export default abstract class AbstractTypeOrmCrudRepository<Entity extends AbstractEntity> implements BaseRepositoryInterface<Entity> {
+
+    constructor(private repository: Repository<Entity>) {}
+    
+    async getById(id: number): Promise<Entity | null> {
+        return this.getRepository().findOneBy({ id } as any)
+    }
+    
+    async listAll(): Promise<Entity[]> {
+       return this.getRepository().find();
+    }
+
+    async save(data: DeepPartial<Entity>): Promise<Entity> {
+       const entity = this.getRepository().create(data)
+       return this.getRepository().save(entity)
+    }
+
+    async deleteById(id: number): Promise<void> {
+        await this.getRepository().delete(id);
+    }
+    
+    protected getRepository() {
+        return this.repository;
+    }
+
+}
