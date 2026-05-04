4f5b8ed00ff646923a6e65db135739948fb115c1
Lucas Do Prado <93302128+LucksBr@users.noreply.github.com>
2026-05-03 23:19:37 -0300

    Corrections to user.controller

--- server/src/user-module/user.controller.ts
+++ server/src/user-module/user.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Controller, Inject, Post } from '@nestjs/common';
 import AbstractCrudController from 'src/core/controller/abstract.crud.controller';
 import User from './model/user.entity';
 import CreateUserDTO from './model/dto/create.user.dto';
 import { USER_SERVICE } from './service/user.service.token';
 import type UserServiceInterface from './service/user.service.interface';
 import UpdateUserDTO from './model/dto/update.user.dto';
+import ResponseUserDTO from './model/dto/response.user.dto';
 
 @Controller('user')
-export default class UserController extends AbstractCrudController<User,CreateUserDTO, UpdateUserDTO> {
+export default class UserController extends AbstractCrudController<User,CreateUserDTO, UpdateUserDTO, ResponseUserDTO> {
   
     constructor(@Inject(USER_SERVICE) private readonly userService: UserServiceInterface) {
         super(userService);
+    }
+
+    protected toResponse(entity: User): ResponseUserDTO {
+        return new ResponseUserDTO(entity.id,entity.email)
     }
     
 }
